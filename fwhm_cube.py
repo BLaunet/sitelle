fde44d027f18a87a32055e35024f7dca52da2e5f
blaunet <blaunet@student.ethz.ch>
2017-09-21 16:43:14 +0200

    Updated versions

--- fwhm_cube.py
+++ fwhm_cube.py
@@ -5,6 +5,7 @@ import numpy as np
 import logging
 from orb.utils import io
 import argparse
+import orb.core
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -26,26 +27,57 @@ def get_parser():
                         default=1100)
     return parser
 
-def generate_fwhm_cube(cube, out_prefix):
+def generate_fwhm_cube(cube, out_prefix, silent=False,
+                        only_bandpass=False):
 
     fwhm_cm1_map = cube.get_fwhm_map()
+
     base_axis = cube.params.base_axis.astype(float)
     irreg_axis = 1e8/base_axis
     reg_axis = np.linspace(irreg_axis[0], irreg_axis[-1], base_axis.shape[0] )
     #We flip it to make it wavelength ascending
     reg_axis = np.flip(reg_axis, 0)
-    fwhm_cube = np.repeat(fwhm_cm1_map[:, :, np.newaxis], cube.dimz, axis=2)
-    for z in range(fwhm_cube.shape[2]):
-        print(z)
-        fwhm_cube[:, :, z] =  fwhm_cm1_map * reg_axis[z]**2/1e8
 
-    h = cube.get_header()
-    path = "{}/{}_{}_fwhm_cube".format(out_prefix,
+    if only_bandpass:
+        #We get extremas of the filter bandpass
+        wl_min, wl_max = list(reversed([1e8/x for x in cube.params.filter_range]))
+        #We conserve only spectrum inside this bandpass
+        i_min = np.searchsorted(reg_axis, wl_min)
+        i_max = np.searchsorted(reg_axis, wl_max)
+        reg_axis = reg_axis[i_min:i_max]
+
+    fwhm_cube = np.repeat(fwhm_cm1_map[:, :, np.newaxis], len(reg_axis), axis=2)
+
+    if not silent:
+        progress = orb.core.ProgressBar(fwhm_cube.shape[2])
+    for z in range(fwhm_cube.shape[2]):
+        fwhm_cube[:, :, z] =  fwhm_cm1_map * reg_axis[z]**2/1e8
+        if not silent:
+            progress.update(z)
+    if not silent:
+        progress.end()
+
+    h = gen_wavelength_header(cube, reg_axis)
+
+    ext=''
+    if only_bandpass:
+        ext='_bandpass'
+    path = "{}/{}_{}_fwhm_cube{}".format(out_prefix,
                                     cube.params['object_name'],
-                                    cube.params['filter_name'])
+                                    cube.params['filter_name'],
+                                    ext)
     io.write_fits('%s.fits'%path, fwhm_cube, fits_header=h, overwrite=True)
 
 
+def gen_wavelength_header(cube, axis):
+    h = cube.get_header()
+    h['NAXIS3'] = len(axis)
+    h['CRPIX3'] = 1
+    h['CRVAL3'] = axis[0]
+    h['CDELT3'] = axis[1]-axis[0]
+    h['CUNIT3'] = 'Angstroms'
+    h['CTYPE3'] = 'LINEAR'
+    return h
 
 if __name__ == '__main__':
     parser = get_parser()
@@ -54,4 +86,5 @@ if __name__ == '__main__':
     cube = SpectralCube(path)
 
     out_prefix = args.out_prefix
+
     generate_fwhm_cube(cube, out_prefix)
