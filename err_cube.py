d899cd0ac64dc923bc3beb8c482e8acab8b6229e
blaunet <blaunet@student.ethz.ch>
2017-09-26 16:50:26 +0200

    err cube

--- err_cube.py
+++ err_cube.py
@@ -14,18 +14,6 @@ def get_parser():
     parser.add_argument("-o", "--out_prefix",
                         help="prefix for output path",
                         default='.')
-    parser.add_argument("-xmin", "--xmin",
-                        help="Min index on the x axis, Default = 1000",
-                        default=1000)
-    parser.add_argument("-xmax", "--xmax",
-                        help="Max index on the x axis, Default = 1100",
-                        default=1100)
-    parser.add_argument("-ymin", "--ymin",
-                        help="Min index on the y axis, Default = 1000",
-                        default=1000)
-    parser.add_argument("-ymax", "--ymax",
-                        help="Max index on the y axis, Default = 1100",
-                        default=1100)
     parser.add_argument("-z", "--zdim",
                         help="Dimension along spectral axis. Default = 221 = size for SN2 cube bandpass",
                         default=221)
@@ -34,7 +22,7 @@ def get_parser():
 def generate_errmap_cube(flux_err, z_size, out_prefix='.'):
 
     errcube = np.repeat(flux_err[:, :, np.newaxis], z_size, axis=2)
-    path = "{}/M31_errcube".format(out_prefix)
+    path = "{}/M31_SN3_errcube".format(out_prefix)
     io.write_fits('%s.fits'%path, errcube, overwrite=True)
 
 if __name__ == '__main__':
