a0be4649ae949d0712a1e1eb9b05dd0487ab1489
blaunet <blaunet@student.ethz.ch>
2017-10-06 19:06:42 +0200

    Helpers to parse fit results

---
+++ sitelle/fit.py
@@ -0,0 +1,44 @@
+from orb.fit import Lines
+import pandas as pd
+import numpy as np
+
+def parse_line_params(rest_lines, fit, error = True, wavenumber = True):
+
+    #Potential names
+    lines_cm1 = list()
+    names = False
+    for iline in rest_lines:
+        if isinstance(iline, str):
+            names = True
+            iline = Lines().get_line_cm1(iline)
+        lines_cm1.append(iline)
+    lines_cm1 = np.array(lines_cm1)
+
+    lines_list = []
+    for i in range(len(lines_cm1)):
+        line = {}
+        if names:
+            line['name'] = rest_lines[i]
+        line['rest_pos'] = lines_cm1[i]
+
+        line['height'], line['amp'], line['pos'], line['fwhm'], line['sigma'] = fit['lines_params'][i]
+        line['velocity'] = fit['velocity'][i]
+        line['snr'] = fit['snr'][i]
+        line['flux'] = fit['flux'][i]
+        line['broadening'] = fit['broadening'][i]
+
+        if error:
+            line['height_err'], line['amp_err'], line['pos_err'], line['fwhm_err'], line['sigma_err'] = fit['lines_params_err'][i]
+            line['velocity_err'] = fit['velocity_err'][i]
+            line['broadening_err'] = fit['broadening_err'][i]
+        lines_list.append(line)
+
+    lines_df = pd.DataFrame(lines_list)
+    if not wavenumber:
+        lines_df['pos'] = 1e8/lines_df['pos']
+        lines_df['rest_pos'] = 1e8/lines_df['rest_pos']
+        lines_df['sigma'] = lines_df['sigma']*lines_df['pos']**2/1e8
+        if error:
+            lines_df['pos_err'] = lines_df['pos_err']*lines_df['pos']**2/1e8
+            lines_df['sigma_err'] = lines_df['sigma_err']*lines_df['pos']**2/1e8
+    return lines_df
