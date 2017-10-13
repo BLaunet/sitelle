375a0cc0836f6274854954626c37eb4dd3c34d04
blaunet <blaunet@student.ethz.ch>
2017-10-13 10:09:55 +0200

    Fit sky lines and gas line in a classic way; parallelized

--- scripts/fit_gas_lines.py
+++ scripts/fit_gas_lines.py
@@ -8,9 +8,9 @@ import gvar
 
 SN3 = SpectralCube('../fits/orig/M31_SN3.merged.cm1.1.0.hdf5')
 SN3.correct_wavelength('../fits/M31_SN3.1.0.ORCS/M31_SN3.1.0.skymap.fits')
-theta_binned = io.read_fits('../fits/M31_SN3_thetamap_48x48.fits')
-cube_binned = io.read_fits('../fits/M31_SN3_rebinned_48x48.fits')
-velocity_guess_map_binned = np.load('../fits/velocity_guess_map_48x48.npy')
+theta_binned = io.read_fits('../fits/maps/M31_SN3_thetamap_48x48.fits')
+cube_binned = io.read_fits('../fits/wavenumber_rebinned/M31_SN3_rebinned_48x48_sky_clean.fits')
+velocity_guess_map_binned = np.load('../fits/maps/velocity_guess_map_48x48.npy')
 
 ##Smaller
 #cube_binned = cube_binned[:10, :10, :]
@@ -25,13 +25,23 @@ inputparams = SN3.inputparams
 params = SN3.params
 
 def _func_vec(sub_cube, sub_theta_map, sub_V_map, inputparams, params, lines):
+    def homemade_chi2(axis, spectrum, fit_params):
+        indices_to_keep = []
+        for line in fit_params['fitted_models']['Cm1LinesModel']:
+            indices_to_keep.append(np.argwhere(np.abs(line)>=line.std()/2)[:,0])
+        to_keep = np.unique(np.concatenate(indices_to_keep))
+        sigma = np.std(spectrum)
+        residual = (spectrum-fit_params['fitted_vector'])[to_keep]
+        return np.sum(np.square(residual/sigma))
+
     line_spectra = np.zeros(sub_cube.shape, dtype=float)
     fit_params = np.zeros(sub_cube.shape[:2], dtype=object)
     for x, y in np.ndindex(sub_cube.shape[:2]):
-        print x,y
+        chi2_list = []
         fit = fit_lines_in_spectrum(params, inputparams, 1e10, sub_cube[x, y, :],
-                              sub_theta_map[x, y], pos_cov=sub_V_map[x,y], snr_guess=None)
+                              sub_theta_map[x, y], pos_cov=gvar.gvar(sub_V_map[x,y], 50), snr_guess=None)
         if fit != []:
+
             line_spectra[x, y, :] = np.sum(fit['fitted_models']['Cm1LinesModel'], 0)
             fit_params[x,y] = parse_line_params(lines,fit)
     return line_spectra, fit_params
@@ -53,14 +63,14 @@ jobs = [job_server.submit(
                                                               np.array_split(theta_binned, ncpus),
                                                               np.array_split(velocity_guess_map_binned, ncpus))]
 job_output = [job() for job in jobs]
+close_pp_server(job_server)
+
 line_spectra = np.concatenate([j[0] for j in job_output])
 fit_params = np.concatenate([j[1] for j in job_output])
-print line_spectra.shape
 #for job in jobs:
 #    individual_results = job()
 #    print individual_results.shape
 #    res = np.concatenate(individual_results, 0)
 #    print res.shape
-np.save('lines_fit.npy', line_spectra)
-np.save('lines_param.npy', fit_params)
-close_pp_server(job_server)
+np.save('gas_after_sky_lines_fit.npy', line_spectra)
+np.save('gas_after_sky_lines_param.npy', fit_params)
