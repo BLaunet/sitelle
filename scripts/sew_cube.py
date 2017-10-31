51dc30b0c5a97823dd194ad40c41b451e19b006d
blaunet <blaunet@student.ethz.ch>
2017-10-31 16:00:30 +0100

    Sew Cube largement simplifié

--- scripts/sew_cube.py
+++ scripts/sew_cube.py
@@ -8,54 +8,33 @@ from scipy.interpolate import UnivariateSpline
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dir",
-                        help="path to files directory")
     parser.add_argument("-o", "--out_prefix",
                         help="prefix for output path",
-                        default='.')
+                        default='./')
     parser.add_argument("-t", "--type",
                         help="type of the cube",
                         default = "CUBE")
     return parser
 
-def sew(files_dir, type, out_prefix, on_rebinned=True):
-    if type == 'CUBE':
-        hdu2 = fits.open(files_dir+'M31_SN2_rebinned_48_cube.fits')[0]
-        hdu3 = fits.open(files_dir+'M31_SN3_rebinned_48_cube.fits')[0]
-        #hdu2 = fits.open(files_dir+'M31_SN2_wl_grid_bandpass.fits')[0]
-        #hdu3 = fits.open(files_dir+'M31_SN3_wl_grid_bandpass.fits')[0]
-    elif type == 'FWHM':
-        hdu2 = fits.open(files_dir+'M31_SN2_rebinned_48_fwhm.fits')[0]
-        hdu3 = fits.open(files_dir+'M31_SN3_rebinned_48_fwhm.fits')[0]
-        #hdu2 = fits.open(files_dir+'M31_SN2_fwhm_cube_bandpass.fits')[0]
-        #hdu3 = fits.open(files_dir+'M31_SN3_fwhm_cube_bandpass.fits')[0]
+def sew(SN2_filename, SN3_filename, type, out_prefix, on_rebinned=True):
 
-    SN2_wl_grid = hdu2.data
-    SN3_wl_grid = hdu3.data
+    hdu2 = fits.open(SN2_filename)[0]
+    hdu3 = fits.open(SN3_filename)[0]
+    SN2_wl_grid = hdu2.data.T
+    SN3_wl_grid = hdu3.data.T
 
-    if on_rebinned:
-        hdu2 = fits.open(files_dir+'M31_SN2_rebinned_48_cube.fits')[0]
-        hdu3 = fits.open(files_dir+'M31_SN3_rebinned_48_cube.fits')[0]
     SN2_h = hdu2.header
     SN3_h = hdu3.header
 
-    if on_rebinned:
-        SN2_axis = SN2_h['CRVAL1'] + SN2_h['CDELT1']*np.arange(0,SN2_h['NAXIS1'])
-        SN3_axis = SN3_h['CRVAL1'] + SN3_h['CDELT1']*np.arange(0,SN3_h['NAXIS1'])
-        step = SN2_h['CDELT1']
-    else:
-        SN2_axis = SN2_h['CRVAL3'] + SN2_h['CDELT3']*np.arange(0,SN2_h['NAXIS3'])
-        SN3_axis = SN3_h['CRVAL3'] + SN3_h['CDELT3']*np.arange(0,SN3_h['NAXIS3'])
-        step = SN2_h['CDELT3']
+    SN2_axis = SN2_h['CRVAL1'] + SN2_h['CDELT1']*np.arange(0,SN2_h['NAXIS1'])
+    SN3_axis = SN3_h['CRVAL1'] + SN3_h['CDELT1']*np.arange(0,SN3_h['NAXIS1'])
 
+    step = SN2_h['CDELT1']
     new_axis = np.arange(SN2_axis.min(), SN3_axis.max(), step=step) #We choose the largest step (lower resolution)
 
     SN2_max_id = np.searchsorted(new_axis,SN2_axis.max(), side='right')
     SN3_min_id = np.searchsorted(new_axis,SN3_axis.min())
 
-    if on_rebinned:
-        SN2_wl_grid = SN2_wl_grid.T
-        SN3_wl_grid = SN3_wl_grid.T
 
     new_cube = np.zeros( (len(new_axis), SN2_wl_grid.shape[1], SN2_wl_grid.shape[2]) )
 
@@ -73,44 +52,44 @@ def sew(files_dir, type, out_prefix, on_rebinned=True):
                                                         old_axis=SN3_axis,
                                                         new_axis=new_axis[SN3_min_id:])
 
-    if on_rebinned:
-        new_cube = new_cube.T
 
-    hdu = fits.PrimaryHDU(new_cube, header=SN2_h)
+    hdu = fits.PrimaryHDU(new_cube.T, header=SN2_h)
     del new_cube
 
-    hdu.writeto(out_prefix+'M31_SN2-3_%s.fits'%str.lower(type), overwrite=True)
+    hdu.writeto(out_prefix+'M31_SN2-3_fwhm_cube.fits', overwrite=True)
     del hdu
 
-    if type == 'CUBE':
-        hdu2 = fits.open(files_dir+'M31_SN2_rebinned_48_err.fits')[0]
-        hdu3 = fits.open(files_dir+'M31_SN3_rebinned_48_err.fits')[0]
-        #hdu2 = fits.open(files_dir+'M31_SN2_errcube.fits')[0]
-        #hdu3 = fits.open(files_dir+'M31_SN3_errcube.fits')[0]
-
-        new_err_cube = np.zeros((len(new_axis), SN2_wl_grid.shape[1], SN2_wl_grid.shape[2]))
-
-
-        if on_rebinned:
-            SN2_err = hdu2.data.T
-            SN3_err = hdu3.data.T
-        else:
-            SN2_err = hdu2.data
-            SN3_err = hdu3.data
-
-        new_err_cube[:SN2_max_id, :, :] = SN2_err
-        new_err_cube[SN3_min_id:, :, :] = SN3_err[:len(new_axis[SN3_min_id:]),:,:]
-
-        if on_rebinned:
-            new_err_cube = new_err_cube.T
-        hdu = fits.PrimaryHDU(new_err_cube, header=SN2_h)
-        del new_err_cube
-        hdu.writeto(out_prefix+'M31_SN2-3_errcube.fits', overwrite=True)
+    # if type == 'CUBE':
+    #     hdu2 = fits.open(files_dir+'M31_SN2_rebinned_48_err.fits')[0]
+    #     hdu3 = fits.open(files_dir+'M31_SN3_rebinned_48_err.fits')[0]
+    #     #hdu2 = fits.open(files_dir+'M31_SN2_errcube.fits')[0]
+    #     #hdu3 = fits.open(files_dir+'M31_SN3_errcube.fits')[0]
+    #
+    #     new_err_cube = np.zeros((len(new_axis), SN2_wl_grid.shape[1], SN2_wl_grid.shape[2]))
+    #
+    #
+    #     if on_rebinned:
+    #         SN2_err = hdu2.data.T
+    #         SN3_err = hdu3.data.T
+    #     else:
+    #         SN2_err = hdu2.data
+    #         SN3_err = hdu3.data
+    #
+    #     new_err_cube[:SN2_max_id, :, :] = SN2_err
+    #     new_err_cube[SN3_min_id:, :, :] = SN3_err[:len(new_axis[SN3_min_id:]),:,:]
+    #
+    #     if on_rebinned:
+    #         new_err_cube = new_err_cube.T
+    #     hdu = fits.PrimaryHDU(new_err_cube, header=SN2_h)
+    #     del new_err_cube
+    #     hdu.writeto(out_prefix+'M31_SN2-3_errcube.fits', overwrite=True)
 
 if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
-    files_dir = args.dir
     out_prefix = args.out_prefix
     type = str.upper(args.type)
-    sew(files_dir, type, out_prefix)
+
+    SN2 = '/Users/blaunet/Documents/M31/nburst/SN2/cube/M31_SN2_fwhm_cube_48x48.fits'
+    SN3 = '/Users/blaunet/Documents/M31/nburst/SN3/cube/M31_SN3_fwhm_cube_48x48.fits'
+    sew(SN2, SN3, type, out_prefix)
