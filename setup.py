72d2bccbe8415ba22b10734388d79d954165cd6d
blaunet <blaunet@student.ethz.ch>
2017-11-07 17:52:05 +0100

    Adapted to be pip installed

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup, find_packages
+from sitelle import version
 
 packages = find_packages(where=".")
-setup(name="sitelle",
-      packages=packages,
+setup(
+    name="sitelle",
+    packages=packages,
+    url='https://gitlab.ethz.ch/blaunet/sitelle',
+    author='Barthelemy Launet',
+    author_email='barthelemy.launet@obspm.fr',
+    maintainer='Barthelemy Launet',
+    maintainer_email='barthelemy.launet@obspm.fr',
+    version=version.__version__,
+    description='Helper functions to work with M31 observations on SITELLE',
+    include_package_data=False,
+    platforms='any',
 )
