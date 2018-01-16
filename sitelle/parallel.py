37faf1a5a1abf42b96b882f3f9172bddb152dfb0
Barthélémy Launet <blaunet@student.ethz.ch>
2018-01-16 15:30:24 +0100

    Added parallel apply over frames : great !

--- sitelle/parallel.py
+++ sitelle/parallel.py
@@ -14,7 +14,7 @@ def parallel_apply_along_axis(func1d, axis, arr, *args, **kwargs):
     job_server, ncpus = init_pp_server(multiprocessing.cpu_count(),silent=False)
 
     chunks = [(func1d, axis, sub_arr, args, kwargs)
-              for sub_arr in np.array_split(arr, ncpus)]
+              for sub_arr in np.array_split(arr, ncpus, axis=((axis + 1 )% 3))]
 
     def helper(func1d, axis, arr, args, kwargs):
         return np.apply_along_axis(func1d, axis, arr, *args, **kwargs)
@@ -35,6 +35,65 @@ def parallel_apply_along_axis(func1d, axis, arr, *args, **kwargs):
     close_pp_server(job_server)
 
     return np.concatenate(job_output)
+
+def parallel_apply_over_frames(func2d, cube, *args, **kwargs):
+    """
+    Iterates over the z dimension of a cube to apply a function on each frame,
+    taking advantage of multiple cores.
+
+    :param func2d: the function to apply to each frame
+    :param cube: the data cube
+    :param modules: modules to be imported so that the function works correctly. Example : 'import numpy as np'
+    :param depfuncs: functions used by the main function but defined outside of its body
+    :param args: arguments to be passed to func2d
+    :param kwargs: keyword arguments to be passed to func2d
+    """
+    try:
+        if len(cube.shape) != 3:
+            raise ValueError('Shape of the cube must be 3 dimensional; got %s instead'%(cube.shape))
+    except AttributeError as e:
+        raise TypeError('Incorrect type for the data cube : %s'%type(a))
+
+
+    job_server, ncpus = init_pp_server(multiprocessing.cpu_count(),silent=False)
+    if cube.shape[-1]<ncpus:
+        ncpus=cube.shape[-1]
+    chunks = [(func2d, sub_cube, args, kwargs)
+              for sub_cube in np.array_split(cube, ncpus, axis=2)]
+
+    def helper(func2d, cube, args, kwargs):
+        #We have to determine dimension of what is returned
+        res0 = np.array(func2d(cube[:,:,0], *args, **kwargs))
+        return cube
+        # return res0.shape
+        # res_cube = np.zeros((res0.shape + (cube.shape[-1], )))
+        # for i in range(1,(cube.shape[-1])):
+        #     res_cube[...,i] = func2d(cube[:,:,i], *args, **kwargs)
+        # return res_cube
+
+    modules = kwargs.pop('modules', tuple())
+    modules += ('import numpy as np',)
+
+    depfuncs = kwargs.pop('depfuncs', tuple())
+    depfuncs += (func2d,)
+    jobs = [job_server.submit(
+                helper,
+                args=(c),
+                modules=(modules),
+                depfuncs=(depfuncs))
+                for c in chunks]
+
+    job_output = [job() for job in jobs]
+    close_pp_server(job_server)
+    return job_output
+    try:
+        return np.concatenate(job_output, axis=-1)
+    except Exception as e:
+        print 'Could not concatenate'
+        return job_output
+
+
+
 
 def parallel_loop(func, iterator, *args, **kwargs):
     job_server, ncpus = init_pp_server(multiprocessing.cpu_count(),silent=False)
