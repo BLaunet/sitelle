3a5daae2de32135a1e132702d66f1108603c9661
Barthélémy Launet <blaunet@student.ethz.ch>
2018-01-16 15:29:17 +0100

    Adding constants to simplify file organization accross machines

--- sitelle/constants.py
+++ sitelle/constants.py
@@ -1,4 +1,14 @@
 from astropy.coordinates import SkyCoord
 import astropy.units as u
+from path import Path
+import socket
 
 M31_CENTER = SkyCoord(ra='00h42m44.371s', dec='41d16m08.34s')#, distance=778*u.kpc)
+
+if 'MacBookAirdeBarthelemy' in socket.gethostname():
+    FITS_DIR = Path('/home/blaunet/Documents/M31/fits/')
+
+if 'johannes' in socket.gethostname() or 'tycho' in socket.gethostname():
+    FITS_DIR = Path('/data/blaunet/fits/')
+if 'lp-alm-stg' in socket.gethostname():
+    FITS_DIR = Path('/Users/blaunet/Documents/M31/fits')
