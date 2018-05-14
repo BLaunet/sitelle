db9f71963b6ccf6865c4cf479a71f64fec5d0ad7
Barthélémy Launet <blaunet@student.ethz.ch>
2018-05-14 13:48:07 +0200

    Parallel doc updated

--- sitelle/parallel.py
+++ sitelle/parallel.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import multiprocessing
 import numpy as np
 from orb.utils.parallel import init_pp_server, close_pp_server
@@ -8,10 +10,13 @@ import re
 import subprocess
 
 __all__ = ['available_cpu_count', 'parallel_apply_along_axis', 'apply_over_frames', 'parallel_apply_over_frames', 'parallel_apply_over_df']
+
 def available_cpu_count():
-    """ Number of available virtual or physical CPUs on this system, i.e.
+    """
+    Number of available virtual or physical CPUs on this system, i.e.
     user/real as output by time(1) when called with an optimally scaling
-    userspace-only program"""
+    userspace-only program
+    """
 
     # cpuset
     # cpuset may restrict the number of *available* processors
@@ -122,12 +127,35 @@ def available_cpu_count():
 
 def parallel_apply_along_axis(func1d, axis, arr, *args, **kwargs):
     """
-    Like numpy.apply_along_axis(), but takes advantage of multiple
+    Apply a function to 1-D slices along the given axis.
+    Like :func:`numpy:numpy.apply_along_axis()`, but takes advantage of multiple
     cores.
-    -------------
-    Additional keywords :
-    :param modules: modules to be imported so that the function works correctly. Example : 'import numpy as np'
-    :param depfuncs: functions used by the main function but defined outside of its body
+
+    Parameters
+    ----------
+    func1d : callable
+        This function should accept 1-D arrays. It is applied to 1-D slice of **arr** along the specified axis.
+    axis : integer
+        Axis along which **arr** is sliced.
+    arr : :class:`~numpy:numpy.ndarray`
+        Input array
+    modules : tuple of strings
+        The modules to be imported so that **func1d** works correctly. Example : ('import numpy as np',)
+    depfuncs : tuple of string
+        The functions used by **func1d** but defined outside of its body
+    args :
+        Additional arguments to be passed  to **func1d**
+    kwargs :
+        Additional keywords arguments to be passed  to **func1d**
+
+    Returns
+    -------
+    out : :class:`~numpy:numpy.ndarray`
+        The output array. The shape of out is identical to the shape of **arr**, except along the **axis** dimension. This axis is removed, and replaced with new dimensions equal to the shape of the return value of **func1d**. So if **func1d** returns a scalar out will have one fewer dimensions than **arr**.
+
+    See Also
+    --------
+    :func:`numpy:numpy.apply_along_axis()`
     """
     job_server, ncpus = init_pp_server(available_cpu_count(),silent=False)
 
@@ -159,6 +187,25 @@ def parallel_apply_along_axis(func1d, axis, arr, *args, **kwargs):
     return np.concatenate(job_output)
 
 def apply_over_frames(func2d, cube, *args, **kwargs):
+    """
+    Iterates over the z dimension of a cube to apply a function on each frame (i.e. 2D images)
+
+    Parameters
+    ----------
+    func2d : callable
+        This function should accept 2-D arrays. It is applied to 2-D slices (frames) of **cube**.
+    cube : 3-D :class:`~numpy:numpy.ndarray`
+        The data cube. Dimensions should be [x,y,z]. **func2d** will be applied along the z dimension
+    args :
+        Additional arguments to be passed  to **func2d**
+    kwargs :
+        Additional keywords arguments to be passed  to **func2d**
+
+    Returns
+    -------
+    out : :class:`~numpy:numpy.ndarray`
+        The output array. The first 2 dimensions are identical to **cube**. the rest depends on what **func2d** returns; it it returns a vector, **out** will be 3-D.
+    """
     #We have to determine dimension of what is returned
     res0 = np.array(func2d(cube[:,:,0], *args, **kwargs))
     res_cube = np.zeros((res0.shape + (cube.shape[-1], )))
@@ -170,15 +217,31 @@ def apply_over_frames(func2d, cube, *args, **kwargs):
 
 def parallel_apply_over_frames(func2d, cube, *args, **kwargs):
     """
-    Iterates over the z dimension of a cube to apply a function on each frame,
-    taking advantage of multiple cores.
+    Iterates over the z dimension of a cube to apply a function on each frame (i.e. 2D images), taking advantage of multiple cores.
 
-    :param func2d: the function to apply to each frame
-    :param cube: the data cube
-    :param modules: modules to be imported so that the function works correctly. Example : 'import numpy as np'
-    :param depfuncs: functions used by the main function but defined outside of its body
-    :param args: arguments to be passed to func2d
-    :param kwargs: keyword arguments to be passed to func2d
+    Parameters
+    ----------
+    func2d : callable
+        This function should accept 2-D arrays. It is applied to 2-D slices (frames) of **cube**.
+    cube : 3-D :class:`~numpy:numpy.ndarray`
+        The data cube. Dimensions should be [x,y,z]. **func2d** will be applied along the z dimension
+    modules : tuple of strings
+        The modules to be imported so that **func2d** works correctly. Example : ('import numpy as np',)
+    depfuncs : tuple of string
+        The functions used by **func2d** but defined outside of its body
+    args :
+        Additional arguments to be passed  to **func2d**
+    kwargs :
+        Additional keywords arguments to be passed  to **func2d**
+
+    Returns
+    -------
+    out : :class:`~numpy:numpy.ndarray`
+        The output array. The first 2 dimensions are identical to **cube**. the rest depends on what **func2d** returns; it it returns a vector, **out** will be 3-D.
+
+    See Also
+    --------
+    `apply_over_frames`
     """
     try:
         if len(cube.shape) != 3:
@@ -220,13 +283,31 @@ def parallel_apply_over_df(df, func, axis=1, broadcast=False, raw=False, reduce=
     """
     Iterates over a pandas Dataframe to apply a function on each line,
     taking advantage of multiple cores.
-    Arguments are the same as pd.DataFrame.apply()
-    :param df: the dataframe
-    :param func: the function to apply to each line (should take a pandas Series as input)
-    :param modules: modules to be imported so that the function works correctly. Example : 'import numpy as np'
-    :param depfuncs: functions used by the main function but defined outside of its body
-    :param args: arguments to be passed to func
-    :param kwargs: keyword arguments to be passed to func
+    The signature is similar to :func:`pandas:pandas.DataFrame.apply()`
+
+    Parameters
+    ----------
+    df : :class:`~pandas:pandas.DataFrame`
+        The input dataframe
+    func : callable
+        The function to apply to each line (should accept a :class:`~pandas:pandas.Series` as input)
+    axis : {0 or 'index', 1 or 'columns'}, default 0
+        | 0 or 'index': apply function to each column
+        | 1 or 'columns': apply function to each row
+    broadcast : boolean, default False
+        For aggregation functions, return object of same size with values propagated
+    raw : boolean, default False
+        If False, convert each row or column into a Series. If raw=True the passed function will receive ndarray objects instead. If you are just applying a NumPy reduction function this will achieve much better performance
+    reduce : boolean or None, default None
+        Try to apply reduction procedures. If the DataFrame is empty, apply will use reduce to determine whether the result should be a Series or a DataFrame. If reduce is None (the default), apply’s return value will be guessed by calling func an empty Series (note: while guessing, exceptions raised by func will be ignored). If reduce is True a Series will always be returned, and if False a DataFrame will always be returned
+    modules : tuple of strings
+        The modules to be imported so that **func** works correctly. Example : ('import numpy as np',)
+    depfuncs : tuple of string
+        The functions used by **func** but defined outside of its body
+    args :
+        Additional arguments to be passed  to **func**
+    kwargs :
+        Additional keywords arguments to be passed  to **func**
     """
     modules = kwargs.pop('modules', tuple())
     modules += ('import numpy as np','import pandas as pd')
